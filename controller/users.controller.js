da33977eb49c3c464ca873da396797525f5c8941
Meherab211 <meherab395@gmail.com>
2022-09-04 16:11:24 +0600

    ger random api

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -4,3 +4,20 @@ let users = require("./../users.json");
 module.exports.getAllUsers = (req, res) => {
 res.send(users)
 }
+
+//random user
+module.exports.getRandomUser = (req, res) => {
+    res.send(users[Math.floor(Math.random() * users.length)])
+}
+
+// signle user save
+module.exports.saveAuser = (req, res) => {
+
+    const data = req.body;
+    const { id, gender, name, contact, address, photoUrl } = data;
+
+    id && gender && name && contact && address && photoUrl ?
+        users.push(data) && res.send(users)
+        :
+        res.send('Invalid data,plesse  input valid data')
+}
--- routes/useres.route.js
+++ routes/useres.route.js
@@ -3,6 +3,8 @@ const usersControlers = require('../controller/users.controller');
 const router = express.Router();
 
 router.get(('/user'),usersControlers.getAllUsers)
+router.get(('/random'),usersControlers.getRandomUser)
+router.post(('/save'),usersControlers.saveAuser)
 
 
 module.exports = router;
